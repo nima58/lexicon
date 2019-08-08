b87c62d4fe32569b95e0669ad3180ddce8425eb2
admin <admin>
2019-08-08 10:02:54 +0200

    minor refactoring

---
+++ src/main/java/com/example/lexicon/rest/utils/Utils.java
@@ -0,0 +1,10 @@
+package com.example.lexicon.rest.utils;
+
+public class Utils {
+    public static final String LEXICON = "lexicon";
+    public static final String POS = "pos";
+    public static final String BCC = "bcc";
+    public static final String BCC_LATIN_COM = "bcc-latin-com";
+    public static final String BCC_LATIN_SCI = "bcc-latin-sci";
+    public static final String SLASH = "/";
+}
